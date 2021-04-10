aa1fe32acef8848763d44b10f2b7ee259032aa63
Sharon Templin <sharon@hollygo.es>
2021-04-09 21:50:06 -0500

    scrape_mars.py working including adding all to mongo db

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,7 +1,7 @@
 import requests
 import pymongo
 from splinter import Browser
-from b24 import BeautifulSoup
+from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 
@@ -10,20 +10,26 @@ def init_browser():
     executable_path = {'executable_path':ChromeDriverManager().install()}
     return Browser('chrome', **executable_path, headless=False)
 
+def init_mongodb():
+    # Initialize PyMongo to work with MongoDBs
+    conn = 'mongodb://localhost:27017'
+    client = pymongo.MongoClient(conn)
+    return client
+
 def scrape():
+    print("Now scraping -----------")
     # NASA Mars News ....
+    print("NASA Mars News")
     # Use the splinter browser... 
     nasa_url = "https://redplanetscience.com/"
-
+    browser = init_browser()
     browser.visit(nasa_url)
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
 
     results = soup.find_all('div', class_='list_text')
 
-    # Initialize PyMongo to work with MongoDBs
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn)
+    client = init_mongodb()
 
     db = client.nasa_db
     collection = db.items
@@ -33,60 +39,72 @@ def scrape():
         try:
             title = result.find('div', class_='content_title').text
             blurb = result.find('div', class_='article_teaser_body').text
-            
-    #         print('**************')
-    #         print(f'Title: {title}')
-    #         print(f'Blurb: {blurb}')
-            
+          
             # gather up the items for mongo db
             if (title and blurb):
-            post = {
-                'title': title,
-                'blurb': blurb,
+                post = {
+                    'title': title,
+                    'blurb': blurb,
                 }
 
             collection.insert_one(post)
         except Exception as e:
             print(e)
-    
-    # JPL Mars Space Images - Featured Image
 
+ 
+    # JPL Mars Space Images - Featured Image
+    print("JPL Mars Space Images - Featured Image")
     jpl_url = "https://spaceimages-mars.com/"
 
     browser.visit(jpl_url)
 
     html = browser.html
     jpl_soup = BeautifulSoup(html, 'html.parser')
-    #jpl_soup
-
-    #jpl_results = jpl_soup.find_all('div', class_='floating_text_area')
+    
     jpl_results = jpl_soup.find_all('img', class_='headerimage fade-in')
 
     featured_image_url = f"{jpl_url}{jpl_results[0]['src']}"
-    #featured_image_url
+
+    try:
+        post = {'featured_image_url':featured_image_url}
+        collection.insert_one(post)
+
+    except Exception as e:
+        print(e) 
+    
 
     # Mars Facts
+    print("Mars Facts from galaxyfacts-mars.com")
     mars_facts_url = 'https://galaxyfacts-mars.com/'
 
     tables = pd.read_html(mars_facts_url)
-    #tables
+    
     mars_earth_table_df = tables[0]
-    #mars_earth_table_df
-
     mars_profile_table_df = tables[1]
+    
     me_list = mars_earth_table_df.loc[0]
     mars_earth_table_df.columns = me_list
-
-    #mars_earth_table_df
-
-    print(mars_earth_table_df.to_html())
-
+   
     mp_list =["Mars Attribute", "Measurement"]
     mars_profile_table_df.columns = mp_list
+    
+    try:  
+        # gather up the items for mongo db
+        html_table = mars_earth_table_df.to_html()
+        print(html_table)
+        post = {'mars_earth_table_html': html_table}
+        collection.insert_one(post)
+        
+        html_table = mars_profile_table_df.to_html()
+        print(html_table)
+        post = {'mars_profile_table_html': html_table}
+        collection.insert_one(post)
+    except Exception as e:
+        print(e)
 
-    print(mars_profile_table_df.to_html())
 
     # Mars Hemispheres  - high resolutions images for each of Mars' hemispheres
+    print("Mars Hemispheres  - high resolutions images for each of Mars' hemispheres")
     hemi_base_url = "https://marshemispheres.com/images/"
 
     # Valles Marineris
@@ -108,8 +126,18 @@ def scrape():
         {"title": "Syrtis Major Hemisphere", "img_url": syrtis_major_url},
     ]
 
-    hemisphere_image_urls
+    try:  
+        # gather up the items for mongo db
+        post = {'hemisphere_image_urls': hemisphere_image_urls}
+        collection.insert_one(post)
+
+    except Exception as e:
+        print(e)
+
+# Let's do the thing
+print('scrape_mars.py now running.... ')
+
+scrape()
 
 
 
-    
