53a19a60d88255c673cb834782d7d53237fe36a8
Sharon Templin <sharon@hollygo.es>
2021-04-09 22:11:07 -0500

    Initial add of index.html.  app.pyf

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import scrape_surfing
+import scrape_mars
 
 app = Flask(__name__)
 
-mongo = PyMongo(app, uri="mongodb://localhost:27017/###")
+mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_mission")
 
 @app.route('/')
 def index():
-    return render_template('index.html', surfing=surfing)
+    mars = mongo.db.mars.find_one()
+    return render_template('index.html', mars=mars)
 
 @app.route('/scrape')
 def scrape():
-    surfing = mongo.db.surfing
-    data = scrape_surfing.scrape()
-    surfing.update(
+    mars = mongo.db.nasa_db
+    data = scrape_mars.scrape()
+    mars.update(
         {},
         data,
         upsert=True
